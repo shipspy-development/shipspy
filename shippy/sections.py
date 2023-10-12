f0d49610104f221ee0183c0ea88b831bd6cef741
laura.koehler <laura.koehler@mpimet.mpg.de>
2023-10-12 15:01:26 +0200

    Transfer script add_section.py to sections subcommand
    Subcommand sections can now do the same as the add_sections script before.

--- shippy/sections.py
+++ shippy/sections.py
@@ -1,8 +1,6 @@
 import numpy as np
 import xarray as xr
 import pandas as pd
-import argparse
-#from .helpers import get_args
 
 def configure_sections_parser(parser):
     parser.add_argument("-i", "--inputfile", metavar="INPUT_FILE",
@@ -31,7 +29,35 @@ def configure_sections_parser(parser):
 
 def _run(args):
     infilename = args.inputfile
+    ds = xr.open_dataset(infilename)
     outfilename = args.outputfile
     secfilename = args.sectionfile
     dim_name = args.dimensioname
-    print(dim_name)
+    
+    complete_period = pd.read_csv(secfilename, names = ['dates'])
+    sct_header = np.argwhere(complete_period.dates.values == 'section_number;section_cuts').flatten()[0]
+    cutout_header = np.argwhere(complete_period.dates.values == 'cutout_start;cutout_end').flatten()[0]
+    cutouts = pd.read_csv(secfilename, header = cutout_header, delimiter = ';')[:sct_header-cutout_header-1]
+    for cut_start, cut_end in zip(cutouts.cutout_start.values, cutouts.cutout_end.values):
+        ds_before = ds.sel({dim_name: slice(None, cut_start)})
+        ds_after = ds.sel({dim_name: slice(cut_end, None)})
+        ds = xr.concat([ds_before, ds_after], dim_name)
+        
+    sections = pd.read_csv(secfilename, header = sct_header, delimiter = ';')
+    sec_cuts = sections['section_cuts'].values
+    sec_nums = sections['section_number'].values
+    section_coords = np.zeros(len(ds.sel({dim_name: slice(None,np.datetime64(sec_cuts[0])-np.timedelta64(1,'s'))})[dim_name]))
+    for i,n in zip(np.arange(len(sec_cuts[-1])), sec_nums[:-1]):
+        sec = n*np.ones(len(ds.sel({dim_name: slice(np.datetime64(sec_cuts[i]),np.datetime64(sec_cuts[i+1])-np.timedelta64(1,'s'))})[dim_name]))
+        section_coords = np.append(section_coords, sec)
+    sec = sec_nums[-1]*np.ones(len(ds.sel({dim_name: slice(np.datetime64(sec_cuts[-1]),None)})[dim_name]))
+    section_coords = np.append(section_coords, sec)
+    section_coords = section_coords.astype('int')    
+    ds = ds.assign_coords(section = (dim_name, section_coords))
+    ds.section.attrs['long_name'] = 'data section'
+    
+    campaign_start = np.datetime64(complete_period['dates'].values[1])
+    campaign_end = np.datetime64(complete_period['dates'].values[3])
+    ds = ds.sel({dim_name: slice(campaign_start,campaign_end)})
+    
+    ds.to_netcdf(outfilename, encoding={dim_name: {'dtype': '<i4', 'units': f'seconds since {campaign_start}'}})
